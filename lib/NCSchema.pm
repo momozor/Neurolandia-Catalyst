3eba66792c39b386109d8ae07268faca8bb88a24
momozor <skelic3@gmail.com>
2019-07-06 23:05:40 +0800

    tidy up

--- lib/NCSchema.pm
+++ lib/NCSchema.pm
@@ -1,4 +1,5 @@
 use utf8;
+
 package NCSchema;
 
 # Created by DBIx::Class::Schema::Loader
@@ -10,11 +11,9 @@ extends 'DBIx::Class::Schema';
 
 __PACKAGE__->load_namespaces;
 
-
 # Created by DBIx::Class::Schema::Loader v0.07049 @ 2019-07-06 23:02:33
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:KvqSu1gmi8dFIH0ZDTyG0Q
 
-
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
-__PACKAGE__->meta->make_immutable(inline_constructor => 0);
+__PACKAGE__->meta->make_immutable( inline_constructor => 0 );
 1;
