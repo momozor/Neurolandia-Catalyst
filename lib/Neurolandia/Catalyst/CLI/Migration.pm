2ab09e3d8fa56155d6ccc5731993256b20fda184
momozor <skelic3@gmail.com>
2019-07-08 01:27:02 +0800

    add blank attributes for migration class

--- lib/Neurolandia/Catalyst/CLI/Migration.pm
+++ lib/Neurolandia/Catalyst/CLI/Migration.pm
@@ -4,6 +4,9 @@ use namespace::autoclean;
 
 extends 'Neurolandia::Catalyst::CLI';
 
+has 'migrate'  => ( 'is' => 'rw', 'isa' => 'Bool', default => 0 );
+has 'rollback' => ( 'is' => 'rw', 'isa' => 'Bool', default => 0 );
+
 __PACKAGE__->meta->make_immutable;
 
 1;
