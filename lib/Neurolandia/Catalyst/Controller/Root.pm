f825ad53e50f23cf31e13a8a5cf7437c9e61bf2d
momozor <skelic3@gmail.com>
2019-07-09 00:55:10 +0800

    add simple authorization for unauthenticated users

--- lib/Neurolandia/Catalyst/Controller/Root.pm
+++ lib/Neurolandia/Catalyst/Controller/Root.pm
@@ -9,6 +9,40 @@ BEGIN { extends 'Neurolandia::Catalyst::Controller' }
 #
 __PACKAGE__->config( namespace => '' );
 
+sub auto : Private {
+    my ( $self, $c ) = @_;
+
+    # Allow unauthenticated users to reach the login page.  This
+    # allows unauthenticated users to reach any action in the Login
+    # controller.  To lock it down to a single action, we could use:
+    #   if ($c->action eq $c->controller('Login')->action_for('index'))
+    # to only allow unauthenticated access to the 'index' action we
+    # added above.
+    if (   ( $c->controller eq $c->controller('User') )
+        || ( $c->action eq $c->controller('Post')->action_for('index') )
+        || ( $c->controller eq $c->controller('Root') ) )
+    {
+        return 1;
+    }
+
+    # If a user doesn't exist, force login
+    if ( !$c->user_exists ) {
+
+        # Dump a log message to the development server debug output
+        $c->log->debug(
+            '***Root::auto User not found, forwarding to /user/login');
+
+        # Redirect the user to the login page
+        $c->response->redirect( $c->uri_for('/user/login') );
+
+    # Return 0 to cancel 'post-auto' processing and prevent use of application
+        return 0;
+    }
+
+    # User found, so return 1 to continue with processing after this 'auto'
+    return 1;
+}
+
 sub index : Path : Args(0) {
     my ( $self, $c ) = @_;
 
