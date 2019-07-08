1352b0d88542fe689b4b78ec811e8e4a1f4120f2
momozor <skelic3@gmail.com>
2019-07-09 01:47:27 +0800

    check if all required parameters are exists

--- lib/Neurolandia/Catalyst/Controller/Post.pm
+++ lib/Neurolandia/Catalyst/Controller/Post.pm
@@ -43,12 +43,15 @@ sub create : Local {
     my $title   = $c->request->params->{title};
     my $content = $c->request->params->{content};
 
-    my $post = $c->model( $self->post_util->current_post_model )->create( {
-        title   => $title,
-        content => $content,
-    } );
-
-    $c->stash( { template => 'post/create_done.html', post => $post } );
+    if ( $title && $content ) {
+        my $post = $c->model( $self->post_util->current_post_model )->create(
+            {
+                title   => $title,
+                content => $content,
+            }
+        );
+        $c->stash( { template => 'post/create_done.html', post => $post } );
+    }
 }
 
 sub edit_form : Chained('/') : PathPart('post/edit_form') : Args(1) {
@@ -66,20 +69,25 @@ sub edit : Local {
     my $title   = $c->request->params->{title};
     my $content = $c->request->params->{content};
 
-    my $post = $self->post_util->get_model_object_by_id( $c, $id );
-    $post->title($title);
-    $post->content($content);
-    $post->update;
+    if ( $id && $title && $content ) {
 
-    $c->stash( { template => 'post/edit_done.html', post => $post } );
+        my $post = $self->post_util->get_model_object_by_id( $c, $id );
+        $post->title($title);
+        $post->content($content);
+        $post->update;
+
+        $c->stash( { template => 'post/edit_done.html', post => $post } );
+    }
 }
 
 sub delete : Chained('/') : PathPart('post/delete') : Args(1) {
     my ( $self, $c, $id ) = @_;
 
-    $self->post_util->get_model_object_by_id( $c, $id )->delete;
+    if ($id) {
+        $self->post_util->get_model_object_by_id( $c, $id )->delete;
 
-    $c->stash( { template => 'post/delete_done.html' } );
+        $c->stash( { template => 'post/delete_done.html' } );
+    }
 }
 
 __PACKAGE__->meta->make_immutable;
