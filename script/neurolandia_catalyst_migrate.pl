5f71fb9ea2d0aa22aaa7e09657bbf04c7ff3e738
momozor <skelic3@gmail.com>
2020-07-04 06:46:58 +0800

    clean

---
+++ script/neurolandia_catalyst_migrate.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+system('sqlite3 ./var/database.db < ./sql/NCSchema.sql');
+#system("./script/neurolandia_catalyst_create.pl model NCModel DBIC::Schema NCSchema create=static 'dbi:SQLite:./var/database.db'");
+
