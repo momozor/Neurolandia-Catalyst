1ab616fb9032054c9539e69a38285e6f475cf33d
momozor <skelic3@gmail.com>
2020-07-04 09:05:37 +0800

    add posts

--- lib/Neurolandia/Catalyst/Controller/Post.pm
+++ lib/Neurolandia/Catalyst/Controller/Post.pm
@@ -24,7 +24,7 @@ Catalyst Controller.
 sub index :Path :Args(0) {
     my ( $self, $c ) = @_;
 
-    $c->stash( {template => 'post/list.tt'} );
+    $c->stash( {template => 'post/list.tt', posts => [$c->model('NCModel::Post')->all]} );
 }
 
 
